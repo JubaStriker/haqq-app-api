f2993fcc46533d184edb5c90b58726a9b248cc47
JubaStriker <jubastriker@gmail.com>
2023-08-24 12:33:00 +0600

    Finalizing deploy

--- cloud/bootstrap/parse-server/index.js
+++ cloud/bootstrap/parse-server/index.js
@@ -3,17 +3,17 @@ const S3Adapter = require("@parse/s3-files-adapter");
 const AWS = require("aws-sdk");
 
 module.exports = {
-	bootstrap() { 
+	bootstrap() {
 		const s3Options = {
 			bucket: process.env.S3_BUCKET_NAME,
 			baseUrl: null,
-			region: process.env.S3_REGION, 
+			region: process.env.S3_REGION,
 			directAccess: true,
 			bucketPrefix: process.env.S3_BUCKET_PREFIX,
 			globalCacheControl: 'public, max-age=31536000'
 		};
 		const s3Adapter = new S3Adapter(s3Options);
-		const api = new ParseServer({ 
+		const api = new ParseServer({
 			databaseURI: process.env.API_SHOPLOOKS_DATABASE_URI,
 			cloud: __dirname + '/main.js',
 			appId: process.env.API_SHOPLOOKS_PARSE_APP_ID,
@@ -25,7 +25,7 @@ module.exports = {
 			verbose: false,
 			filesAdapter: s3Adapter,
 			restApiKey: process.env.API_SHOPLOOKS_PARSE_REST_API_KEY,
-			publicServerURL: process.env.API_SHOPLOOKS_PUBLIC_SERVER_URL + '/parse',//'http://4feea51e.ngrok.io/parse',
+			publicServerURL: process.env.API_SHOPLOOKS_PUBLIC_SERVER_URL + '/parse',//'http://4feea51e.ngrok.io/parse', // https added //
 			readOnlyMasterKey: process.env.API_SHOPLOOKS_READ_ONLY_MASTER_KEY,
 			maxUploadSize: "100mb",
 		});
