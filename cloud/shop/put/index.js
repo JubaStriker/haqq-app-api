6b36d1d7d0e215e2051232d1abe7b48d773187c8
JubaStriker <jubastriker@gmail.com>
2023-10-13 20:10:52 +0600

    Cleaned

--- cloud/shop/put/index.js
+++ cloud/shop/put/index.js
@@ -3,12 +3,10 @@ const parseUtils = require("../../utils/parse-utils/index");
 const { exists } = require("../../utils/validate");
 
 module.exports = {
-  put_shop: async ({ params, user }) => {
+  put_shop: async ({ params }) => {
     const { shop, walletAddress, walletToken, blockchain } = params;
-    console.log(params)
     if (exists(shop)) {
       try {
-        console.log("BLCHN", blockchain)
         const shopQuery = parseUtils.query("Shop");
         shopQuery.equalTo("shop", shop);
         const shopInstance = await shopQuery.first({ useMasterKey: true });
@@ -19,7 +17,6 @@ module.exports = {
           const savedShopInstance = await shopInstance.save(null, {
             useMasterKey: true,
           });
-          console.log(savedShopInstance)
           return savedShopInstance;
         } else {
           const { code, message } = errors.constructErrorObject(404);
